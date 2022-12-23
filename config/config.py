63c615a5a1e8e70f9363ae132a7941ee6fd92cef
alielsayed13 <121264124+alielsayed13@users.noreply.github.com>
2022-12-23 11:13:44 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/alielsayed76/YukkiMusicBot",
+    "https://github.com/alielsayed13/aboelmagedMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
